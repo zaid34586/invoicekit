95f5da2111de55928116f6b7d2d9017912f576e6
Mohd zaid <mz7123272@gmail.com>
2026-08-27 15:06:39 +0530

    feat: shorter professional hero tagline, dedicated About section for detailed positioning

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,6 +4,7 @@ import HowItWorks from "../components/landing/HowItWorks";
 import Pricing from "../components/landing/Pricing";
 import Navbar from "../components/landing/Navbar";
 import Hero from "../components/landing/Hero";
+import About from "../components/landing/About";
 import Features from "../components/landing/Features";
 import DynamicGrowthBanner from "../components/DynamicGrowthBanner";
 
@@ -13,6 +14,7 @@ export default function Landing() {
   <Navbar />
   <DynamicGrowthBanner placement="landing" />
   <Hero />
+  <About />
   <Features />
   <HowItWorks />
   <Pricing />
